ab650cb879ca759573920bbd4963a89ce6035bfd
genesis-g101 <gyreplicant@gmail.com>
2022-10-25 12:29:29 -0300

    passando dados via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
 // O nome do arquivo do componente deve ser o mesmo da função do componente
 // Um componente em react é apenas uma função em javascript.
-export const ExpenseItem = () => {
-  return <h2>Expense Item</h2>;
+import './ExpenseItem.css';
+export const ExpenseItem = ({ props }) => {
+  /*o nome do parametro deve ser o mesmo nome passado como key no elemento pai */
+  const { title, amount, date } = props;
+
+  return (
+    <div className="expense-item ">
+      <div>{date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price ">{amount}</div>
+      </div>
+    </div>
+  );
 };
