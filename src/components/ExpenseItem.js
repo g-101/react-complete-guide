ccf73315f101045e0af56c7d0dec96a76095e919
genesis-g101 <gyreplicant@gmail.com>
2022-10-25 10:17:30 -0300

    criando o primeiro componente customizado

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+// O nome do arquivo do componente deve ser o mesmo da função do componente
+// Um componente em react é apenas uma função em javascript.
+export const ExpenseItem = () => {
+  return <h2>Expense Item</h2>;
+};
