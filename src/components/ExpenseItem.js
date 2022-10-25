7ca7c5be84d568335dac2df88374744221f3f156
genesis-g101 <gyreplicant@gmail.com>
2022-10-25 13:58:39 -0300

    quebrando componentes em multiplos componentes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 // O nome do arquivo do componente deve ser o mesmo da função do componente
 // Um componente em react é apenas uma função em javascript.
 import './ExpenseItem.css';
+import { ExpenseDate } from './ExpenseDate';
 export const ExpenseItem = ({ props }) => {
   /*o nome do parametro deve ser o mesmo nome passado como key no elemento pai */
   const { title, amount, date } = props;
 
   return (
-    <div className="expense-item ">
-      <div>{date.toDateString()}</div>
+    <div className="expense-item">
+      <ExpenseDate props={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price ">{amount}</div>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
